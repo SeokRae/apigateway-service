8a60455d4ff353dac84332037e1d5325165dbfc0
seokrae <kslbsh@gmail.com>
2021-07-21 18:14:46 +0900

    feat: 인가 필터추가 및 로그인, 회원가입 그 외 경로 테스트

---
+++ src/main/java/com/example/apigatewayservice/filter/CustomAuthorizationHeaderFilter.java
@@ -0,0 +1,92 @@
+package com.example.apigatewayservice.filter;
+
+import io.jsonwebtoken.Jwts;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Slf4j
+@Component
+public class CustomAuthorizationHeaderFilter extends AbstractGatewayFilterFactory<CustomAuthorizationHeaderFilter.Config> {
+
+    private final Environment env;
+
+    @Autowired
+    public CustomAuthorizationHeaderFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHader = request.getHeaders().get(org.springframework.http.HttpHeaders.AUTHORIZATION).get(0);
+            String jwtToken = authorizationHader.replace("Bearer", "");
+
+            if(!isJwtValid(jwtToken)) {
+                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    /**
+     * WebFlux 에서 비동기로 처리하는 단위 값 반환
+     * 스프링 5.0의 WebFlux (단일: Mono, 다중: Flux)
+     *
+     * @param exchange
+     * @param message
+     * @param status
+     * @return
+     */
+    private Mono<Void> onError(ServerWebExchange exchange, String message, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+
+        log.error(message);
+
+        return response.setComplete();
+    }
+
+    private boolean isJwtValid(String jwtToken) {
+        boolean returnValue = true;
+
+        String subject = null;
+
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(env.getProperty("token.secret"))
+                    .parseClaimsJws(jwtToken)
+                    .getBody()
+                    .getSubject();
+
+        } catch (Exception e) {
+            returnValue = false;
+        }
+
+        if(subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+        return returnValue;
+    }
+
+    public static class Config {
+    }
+}
